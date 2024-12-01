d9a323da621ca794f149c292c15dc9c980e8eec7
GiovanniTammaro9 <gio_play@live.it>
2024-12-01 22:13:28 +0100

    promise.allsettled

--- index.js
+++ index.js
@@ -48,3 +48,37 @@ Promise.race([funzione3(), funzione4()])
 .catch((errore) => {
     console.log("errore durante l'esecuzione delle promise", errore);
 });
+//promise con allsettled
+function funzione5() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("risultato della funzione 5");
+        }, 3000);
+    });
+}
+function funzione6() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("risultato della funzione 6");
+        }, 1000);
+    });
+}
+function funzione7() {
+    return new Promise((_, reject) => {
+        setTimeout(() => {
+            reject("errore nella funzione 7");
+        }, 1500);
+    });
+}
+
+Promise.allSettled([funzione5(), funzione6(), funzione7()])
+.then((risultati) => {
+    console.log("tutte le promise sono state completate (risolte o rifiutate)");
+    risultati.forEach((risultato, index) => {
+        if (risultato.status === "fulfilled") {
+            console.log(`promise ${index + 1} risolta: ${risultato.value}`);
+        }else {
+            console.log(`promise ${index + 1} rifiutata: ${risultato.reason}`);
+        }
+    });
+});
