aa1ced839a802d8f83859ff7e0969f86ce9d9a1e
GiovanniTammaro9 <gio_play@live.it>
2024-12-01 21:54:55 +0100

    promise.race

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//promise.all
 function funzione1() {
     return new Promise((resolve) => {
         setTimeout(() => {
@@ -17,6 +18,31 @@ function funzione2() {
 Promise.all([funzione1(), funzione2()])
 .then((risultati) => {
     console.log("entrambe le promise sono risolte");
+    console.log(risultati);
+})
+.catch((errore) => {
+    console.log("errore durante l'esecuzione delle promise", errore);
+});
+//promise.race
+function funzione3() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("risultato della funzione 3");
+        }, 3000);
+    });
+}
+
+function funzione4() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("risultato della funzione 4");
+        }, 1000);
+    });
+}
+
+Promise.race([funzione3(), funzione4()])
+.then((risultati) => {
+    console.log("la prima promise risolta è:");
     console.log(risultati);
 })
 .catch((errore) => {
